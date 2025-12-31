d7bccffcc7a237d6a70c7070ed72fddb5fd7430b
tsannkidz123 <tsannkidz5673@gmail.com>
2025-12-31 10:41:00 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App'; // Change from './src/App' to just './App'
-import './index.css';
+import App from './App';
 
 const container = document.getElementById('root');
 if (container) {
